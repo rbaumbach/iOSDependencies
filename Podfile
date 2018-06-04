c37e578099074a4ca2280d92e43285a4971b82f5
Ryan <github@ryan.codes>
2018-06-04 08:39:58 -0600

    Initial project commit with Ruby Gems, Cocoapods and Carthage dependencies

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+inhibit_all_warnings!
+
+target :Specs do
+    pod 'Specta'
+    pod 'Expecta'
+    pod 'OCMock'
+    pod 'Swizzlean'
+end
---
+++ iOSDependencyTester/iOSDependencyTester.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSDependencyTester.
+FOUNDATION_EXPORT double iOSDependencyTesterVersionNumber;
+
+//! Project version string for iOSDependencyTester.
+FOUNDATION_EXPORT const unsigned char iOSDependencyTesterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSDependencyTester/PublicHeader.h>
