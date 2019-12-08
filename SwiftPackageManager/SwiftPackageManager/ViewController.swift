25064e7bd301cdc6f106691366871a1d78c74ede
Ryan <github@ryan.codes>
2019-12-08 16:17:10 -0700

    Add Swift packages

--- SwiftPackageManager/SwiftPackageManager/ViewController.swift
+++ SwiftPackageManager/SwiftPackageManager/ViewController.swift
@@ -1,3 +1,15 @@
 import UIKit
+import Capsule
+import Utensils
 
-class ViewController: UIViewController { }
+class ViewController: UIViewController {
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let trunk = Trunk()
+        
+        trunk.save(data: [0, 1, 2, 3, 4])
+    }
+}
