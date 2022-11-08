c838d56d47020a8bcc0ce3ce8179fbc4fcd847ee
Ryan Baumbach <github@ryan.codes>
2022-11-07 22:21:25 -0700

    Update SwiftPackageManager project

--- SwiftPackageManager/SwiftPackageManager/AppDelegate.swift
+++ SwiftPackageManager/SwiftPackageManager/AppDelegate.swift
@@ -1,8 +1,14 @@
 import UIKit
 
+import Alamofire
+
+import Capsule
+import Utensils
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
