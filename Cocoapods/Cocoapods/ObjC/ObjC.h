96c75c67842e085648d5d319f200f72c883ca6b1
Ryan Baumbach <github@ryan.codes>
2022-11-07 22:34:16 -0700

    Consume more pods

---
+++ Cocoapods/Cocoapods/ObjC/ObjC.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+// Note this was created to have Xcode create a bridging header
+// so that Swizzlean can be imported
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjC : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Cocoapods/Cocoapods/ObjC/ObjC.m
@@ -0,0 +1,5 @@
+#import "ObjC.h"
+
+@implementation ObjC
+
+@end
