ccccbabd2dbded6ab6ee863f28d8dddd9a124ff0
Ryan Baumbach <github@ryan.codes>
2022-11-09 08:05:52 -0700

    Add Swizzlean to SPM project

--- SwiftPackageManager/SwiftPackageManager/AppDelegate.swift
+++ SwiftPackageManager/SwiftPackageManager/AppDelegate.swift
@@ -4,11 +4,15 @@ import Alamofire
 
 import Capsule
 import Utensils
+import Swizzlean
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        _ = Swizzlean(classToSwizzle: NSString.self)
+        
         return true
     }
 }
