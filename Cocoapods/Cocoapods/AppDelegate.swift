5403893acdbea6cd359552555128ec3ebf56e99b
Ryan Baumbach <github@ryan.codes>
2022-11-09 08:02:40 -0700

    Resolve Cocoapods consumption of Swizzlean

--- Cocoapods/Cocoapods/AppDelegate.swift
+++ Cocoapods/Cocoapods/AppDelegate.swift
@@ -4,13 +4,12 @@ import Alamofire
 
 import Capsule
 import Utensils
-//import Swizzlean
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        _ = Swizzlean(classToSwizzle: NSString.self)
+//        _ = Swizzlean(classToSwizzle: NSString.self)
         
         return true
     }
